d25ddda0c3f265443c3ae734942140de6caf550f
Komail Kanjee <kkanjee@eeng.pro>
2021-01-06 00:11:05 -0500

    Adding code of conduct file testing for template project

---
+++ tests/test_code_of_conduct.py
@@ -0,0 +1,19 @@
+"""
+CODE_OF_CONDUCT file testing module for the generated project.
+"""
+import os
+from . import bake_cookie, load_cookiecutter_json
+
+
+def test_code_of_conduct_file(cookies):
+    "Test that the CODE_OF_CONDUCT.md file exists and the content is valid"
+    with bake_cookie(cookies) as result:
+        code_of_conduct_path = result.project.join('CODE_OF_CONDUCT.md')
+
+        assert os.path.exists(code_of_conduct_path)
+
+        code_of_conduct = result.project.join('CODE_OF_CONDUCT.md').read()
+        cookiecutter_json = load_cookiecutter_json()
+
+        assert str(
+            cookiecutter_json['project_author_email']) in code_of_conduct
