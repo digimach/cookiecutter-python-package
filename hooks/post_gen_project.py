f0e8fce384ca73330ba6249f2100f7a45b65bdc0
Komail Kanjee <kkanjee@eeng.pro>
2021-01-09 12:42:35 -0500

    Adding structure around Git operations in hooks/post_gen_project.py and fixing logic
    * Breaking into unit functions for Git operations like init, add and commit
    * Fixing logic to only attempt commit if the repo is dirty
    * Adding the cookiecutter configuration used to create the template in the
      commit message.

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -4,34 +4,52 @@ Cookiecutter post generation hook script that handles operations after the
 template project is used to generate a target project.
 """
 
-import shutil
 import os
+import shlex
+import shutil
 import subprocess
+import sys
+from collections import OrderedDict  # noqa pylint: disable=unused-import
 
 PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
 
 
 def initialize_git_repo():
     """
-    Do a Git init and do a first commit in the generated project.
-    We use git package installed on the target machine vs the gitpython module
-    since that requires the package to be installed on the target machine.
+    Initialize the Git repository in the generated project.
     """
     subprocess.check_output("git init",
                             stderr=subprocess.STDOUT,
                             shell=True,
                             cwd=PROJECT_DIRECTORY)
 
+
+def git_add():
+    """
+    Do a Git add operation on the generated project.
+    """
     subprocess.check_output("git add --all",
                             stderr=subprocess.STDOUT,
                             shell=True,
                             cwd=PROJECT_DIRECTORY)
 
-    commit_message = ("Initial commit - Generated project from cookiecutter"
-                      " template.\n\n"
-                      "Template created from"
+
+def git_commit():
+    """
+    Commit the staged changes in the generated project.
+    """
+
+    cookiecutter_config = {{cookiecutter}}  # noqa pylint: disable=undefined-variable
+
+    cookiecutter_config_str = ""
+    for key, val in cookiecutter_config.items():  # noqa pylint: disable=no-member
+        cookiecutter_config_str += f"  {key}: {val}\n"
+
+    commit_message = ("Template applied from"
                       " https://github.com/Digimach/cookiecutter-python-"
-                      "package")
+                      "package\n\n"
+                      "Template configuration:\n"
+                      f"{cookiecutter_config_str}")
 
     author_info = "{{ cookiecutter.project_author_name }}"
     author_info += " <{{cookiecutter.project_author_email }}>"
@@ -46,10 +64,38 @@ def initialize_git_repo():
                                 shell=True,
                                 cwd=PROJECT_DIRECTORY,
                                 env=env)
+
     except subprocess.CalledProcessError as exc_info:
         if exc_info.returncode != 0:
             print(exc_info.output)
         raise
+
+
+def is_git_repo_clean():
+    """
+    Check to confirm if the Git repository is clean and has no uncommitted
+    changes. If its clean return True otherwise False.
+    """
+    try:
+        if sys.version_info.minor >= 7:
+            git_status = subprocess.run(shlex.split('git status --porcelain'),
+                                        capture_output=True,
+                                        cwd=PROJECT_DIRECTORY,
+                                        check=True)
+        elif sys.version_info.minor < 7:
+            git_status = subprocess.run(shlex.split('git status --porcelain'),
+                                        stdout=subprocess.PIPE,
+                                        stderr=subprocess.PIPE,
+                                        cwd=PROJECT_DIRECTORY,
+                                        check=True)
+    except subprocess.CalledProcessError:
+        print(f"** Git repository in {PROJECT_DIRECTORY} cannot get status")
+        sys.exit(1)
+
+    if git_status.stdout == b'' and git_status.stderr == b'':
+        return True
+
+    return False
 
 
 def set_license():
@@ -75,3 +121,8 @@ if __name__ == '__main__':
 
     if '{{ cookiecutter.initialize_git_repo|lower }}' == 'yes':
         initialize_git_repo()
+        if not is_git_repo_clean():
+            git_add()
+            git_commit()
+
+    sys.exit(0)
