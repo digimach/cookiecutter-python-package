78059125057491dbe2657da7502ae01de9a0637c
Komail Kanjee <kkanjee@eeng.pro>
2021-01-11 23:06:37 -0500

    Moving CODE_OF_CONDUCT to rst format and adding codefactor link * Moving template project CODE_OF_CONDUCT file to rst from md * Updating tests to check rst file * Adding code factor link to cookiecutter project README

--- tests/test_code_of_conduct.py
+++ tests/test_code_of_conduct.py
@@ -8,11 +8,11 @@ from . import bake_cookie, load_cookiecutter_json
 def test_code_of_conduct_file(cookies):
     "Test that the CODE_OF_CONDUCT.md file exists and the content is valid"
     with bake_cookie(cookies) as result:
-        code_of_conduct_path = result.project.join('CODE_OF_CONDUCT.md')
+        code_of_conduct_path = result.project.join('CODE_OF_CONDUCT.rst')
 
         assert os.path.exists(code_of_conduct_path)
 
-        code_of_conduct = result.project.join('CODE_OF_CONDUCT.md').read()
+        code_of_conduct = result.project.join('CODE_OF_CONDUCT.rst').read()
         cookiecutter_json = load_cookiecutter_json()
 
         assert str(
