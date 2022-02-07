d087e963c0eb2dfcf041d053771686c019009c6d
Komail Kanjee <kkanjee@eeng.pro>
2022-02-07 18:56:26 -0500

    Fixing tests to use project_path
    * pytest cookie plugin is deprecating project from result context
    * adjusting code to the new context as needed

--- tests/test_manifest_file.py
+++ tests/test_manifest_file.py
@@ -8,11 +8,10 @@ from . import bake_cookie, load_cookiecutter_json
 def test_manifest_file(cookies):
     "Test that the MANIFEST.in file exists and the content is valid"
     with bake_cookie(cookies) as result:
-        manifest_file_path = result.project.join("MANIFEST.in")
+        manifest_file_path = result.project_path.joinpath("MANIFEST.in")
+        cookiecutter_json = load_cookiecutter_json()
 
         assert os.path.exists(manifest_file_path)
 
-        manifest_file = result.project.join("MANIFEST.in").read()
-        cookiecutter_json = load_cookiecutter_json()
-
-        assert str(cookiecutter_json["project_slug"]) in manifest_file
+        with open(manifest_file_path, "r") as fp:
+            assert str(cookiecutter_json["project_slug"]) in fp.read()
--- tests/test_setup.py
+++ tests/test_setup.py
@@ -3,6 +3,6 @@ from . import bake_cookie, subprocess_in_dir
 
 def test_bake_and_run_tests(cookies):
     with bake_cookie(cookies) as result:
-        assert result.project.isdir()
-        subprocess_in_dir("python setup.py test", str(result.project)) == 0
-        print("python setup.py test", str(result.project))
+        assert result.project_path.is_dir()
+        subprocess_in_dir("python setup.py test", str(result.project_path)) == 0
+        print("python setup.py test", str(result.project_path))
