5d5d01818206071996d7d130090ff368bfea83d6
Komail Kanjee <kkanjee@eeng.pro>
2022-02-07 23:29:58 -0500

    Bringing project up to speed with changes (#28)
    * Using Black in project and rendered project but docs said YAPF.
    * Updating docs and requirement specs to point to black
      for formatting Python code
    * Fixing lint errors in hooks scripts
    * Setting line length limits for flake8 to be 88
    * Removing .yapfignore as yapf is no longer used
    * Updating .gitignore file
    * Fixing tests to use project_path
    * pytest cookie plugin is deprecating project from result context
    * adjusting code to the new context as needed
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    * Fixing issues found by deepsource
    * Moving test workflow to Python setup
    * Using GitHub action Python setup
    * Invoking tox after each Python version is setup
    * Adding pip install tox and upgrade pip
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- tests/test_readme_file.py
+++ tests/test_readme_file.py
@@ -8,5 +8,5 @@ from . import bake_cookie
 def test_readme_file_exists(cookies):
     "Test that the README.md file exists"
     with bake_cookie(cookies) as result:
-        readme_file_path = result.project.join("README.md")
+        readme_file_path = result.project_path.joinpath("README.md")
         assert os.path.exists(readme_file_path)
--- tests/test_sphinx_doc.py
+++ tests/test_sphinx_doc.py
@@ -3,8 +3,8 @@ from . import bake_cookie, subprocess_in_dir
 
 def test_doc_tox_env(cookies):
     with bake_cookie(cookies) as result:
-        assert result.project.isdir()
+        assert result.project_path.is_dir()
         subprocess_in_dir(
             "tox -e docs --workdir .tox",
-            str(result.project),
-        ) == 0
+            str(result.project_path),
+        )
